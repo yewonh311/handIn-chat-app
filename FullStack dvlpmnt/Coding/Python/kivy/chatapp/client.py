1c04e91cd91616d7c43b27a2de424bb49b874883
yewonhong <yewonhong311@gmail.com>
2022-03-28 16:06:28 -0700

    first commit

---
+++ FullStack dvlpmnt/Coding/Python/kivy/chatapp/client.py
@@ -0,0 +1,58 @@
+import socket
+from threading import Thread
+import errno
+
+HEADER_LENGTH = 10
+
+client_socket = None
+
+
+def conn(my_username, error_callback):
+
+    global client_socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    IP = socket.gethostbyname(socket.gethostname())
+    try:
+        client_socket.connect((IP, 8080))
+    except Exception as e:
+        error_callback('Connection error: {}'.format(str(e)))
+        return False
+
+    username = my_username.encode('utf-8')
+    username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(username_header + username)
+
+    return True
+
+
+def send(message):
+    message = message.encode('utf-8')
+    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(message_header + message)
+
+
+def start_listening(incoming_message_callback, error_callback):
+    Thread(target=listen, args=(incoming_message_callback, error_callback),
+           daemon=True).start()
+
+
+def listen(incoming_message_callback, error_callback):
+    while True:
+        try:
+            while True:
+                username_header = client_socket.recv(HEADER_LENGTH)
+
+                if not len(username_header):
+                    error_callback('Connection closed by the server')
+
+                username_length = int(username_header.decode('utf-8').strip())
+                username = client_socket.recv(username_length).decode('utf-8')
+
+                message_header = client_socket.recv(HEADER_LENGTH)
+                message_length = int(message_header.decode('utf-8').strip())
+                message = client_socket.recv(message_length).decode('utf-8')
+
+                incoming_message_callback(username, message)
+
+        except Exception as e:
+            error_callback('Reading error: {}'.format(str(e)))
